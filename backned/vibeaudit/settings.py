069c08b7a1e2822ea59224cb81af03b3b21dd185
Zdzisław Krajewski <zdzislaw.krajewski@ulam.io>
2026-05-14 13:30:37 +0200

    Remove default secret key from settings.py

--- backned/vibeaudit/settings.py
+++ backned/vibeaudit/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 import environ
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,17 +22,16 @@ env = environ.Env()
 environ.Env.read_env(BASE_DIR / ".env")
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env(
-    "SECRET_KEY",
-    default="django-insecure-1du*7f+h9fqd+43^-)756hy7b&^a1_0p)%@+#)wxs%8(nvfpw0",
-)
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
+
+if DEBUG:
+    SECRET_KEY = env("SECRET_KEY", default="changeme")
+else:
+    SECRET_KEY = env("SECRET_KEY", default=None)
+    if not SECRET_KEY:
+        raise ImproperlyConfigured("SECRET_KEY must be set when DEBUG is False.")
+
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
