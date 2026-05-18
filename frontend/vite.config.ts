ee01a11c911c6d009ff1c469373f9dfbd4f807fb
Zdzisław Krajewski <zdzislaw.krajewski@ulam.io>
2026-05-18 07:24:10 +0200

    Fix frontend serving after switching to tanstack.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  base: '/static/',
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
